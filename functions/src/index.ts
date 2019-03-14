9cb6657b58f4f6b19c15a4042c907eb64f720cda
Hugo Arenari <hugoarenari@gmail.com>
2019-03-14 11:16:06 +0000

    deploy with firebase functions

---
+++ functions/src/index.ts
@@ -0,0 +1,19 @@
+const functions = require('firebase-functions');
+const prpl = require('prpl-server');
+const express = require('express');
+const rendertron = require('rendertron-middleware');
+
+const app = express();
+
+const rendertronMiddleware = rendertron.makeMiddleware({
+  proxyUrl: 'https://render-tron.appspot.com/render',
+  injectShadyDom: true,
+});
+
+app.use((req: any, res: any, next: any) => {
+  req.headers['host'] = 'typescript-minimal-ui.firebaseapp.com';
+  return rendertronMiddleware(req, res, next);
+});
+app.get('/*', prpl.makeHandler('./build', require('../build/polymer.json')));
+
+exports.app = functions.https.onRequest(app);
